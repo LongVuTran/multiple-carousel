e14ace65d6691f8a641579a3a6cff40df56fc043
Vu Tran <vutran.270797@gmail.com>
2018-01-07 11:18:43 +0700

    error

---
+++ js/script.js
@@ -0,0 +1,9 @@
+  // $('#carousel-example-generic').on('slid.bs.carousel', function () {
+  //       $(".carousel-item.active:nth-child(" + ($(".carousel-inner .carousel-item").length -1) + ") 
+  //         + .carousel-item").insertBefore($(".carousel-item:first-child"));
+  //       $(".carousel-item.active:last-child").insertBefore($(".carousel-item:first-child"));
+  //   });   
+  
+  $('#carousel-example-generic').carousel({
+  interval: 3000
+})
